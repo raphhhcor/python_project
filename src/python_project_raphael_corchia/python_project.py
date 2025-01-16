3a87bab390841ce7861c5c13623c0569e941189c
raphael_corchia <raphael.corchia@dauphine.eu>
2025-01-16 19:32:25 +0100

    fix: Put all the classes in the same file in order to facilitate the imports.

--- src/python_project_raphael_corchia/python_project.py
+++ src/python_project_raphael_corchia/python_project.py
@@ -1,12 +1,93 @@
+import logging
+from dataclasses import dataclass
 from datetime import datetime
 
+import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
-from . import utils
 from pybacktestchain.blockchain import load_blockchain
-from pybacktestchain.broker import EndOfMonth, StopLoss
+from pybacktestchain.broker import Backtest, Broker, EndOfMonth, StopLoss
+from pybacktestchain.data_module import FirstTwoMoments
+from scipy.optimize import minimize
+
+## Define a custom class of the first two moments one in order to be able to modify some parameters ##
+@dataclass
+class CustomFirstTwoMoments(FirstTwoMoments):
+    gamma: float = 1.0  # Default risk aversion parameter
+    cons: list = None  # Default to None, will define later if not provided
+    bounds: list = None  # Default to None, will define later if not provided
+
+    def __post_init__(self):
+        if self.cons is None:
+            # Default constraint: portfolio weights sum to 1
+            self.cons = [{"type": "eq", "fun": lambda x: np.sum(x) - 1}]
+        if self.bounds is None:
+            # Default bounds: allow short selling
+            self.bounds = [(0.0, 1.0)]  # Change if needed
+
+    def compute_portfolio(self, t: datetime, information_set):
+        try:
+            mu = information_set["expected_return"]
+            Sigma = information_set["covariance_matrix"]
+            n = len(mu)
+
+            # Objective function
+            obj = lambda x: -x.dot(mu) + self.gamma / 2 * x.dot(Sigma).dot(x)  # noqa: E731
+
+            # Initial guess: equal weights
+            x0 = np.ones(n) / n
+
+            # Minimize
+            res = minimize(obj, x0, constraints=self.cons, bounds=self.bounds)
+
+            # Prepare dictionary
+            portfolio = {k: None for k in information_set["companies"]}
+
+            # If optimization converged, update portfolio
+            if res.success:
+                for i, company in enumerate(information_set["companies"]):
+                    portfolio[company] = res.x[i]
+            else:
+                raise Exception("Optimization did not converge")
+
+            return portfolio
+        except Exception as e:
+            # If something goes wrong, return an equal weight portfolio but warn the user
+            logging.warning(
+                "Error computing portfolio, returning equal weight portfolio"
+            )
+            logging.warning(e)
+            return {
+                k: 1 / len(information_set["companies"])
+                for k in information_set["companies"]
+            }
+
+
+## Define a custom class of the backtest one in order to be able to modify some parameters ##
+class CustomBacktest(Backtest):
+    def __init__(
+        self,
+        *args,
+        backtest_name: str = None,
+        initial_cash: float = 1_000_000,
+        **kwargs,
+    ):
+        self.initial_cash = initial_cash
+        self.broker = Broker(cash=self.initial_cash, verbose=self.verbose)
+        super().__init__(*args, **kwargs)
+        # if backtest_name is None, use teh default value of the Backtest class
+        self.backtest_name = (
+            backtest_name if backtest_name is not None else self.backtest_name
+        )
+
+
+def split_text_by_lines(text):
+    # Split the text into lines
+    lines = text.splitlines()
+    # Remove empty lines and strip whitespace
+    return [line.strip() for line in lines if line.strip()]
 
 
 def main():
@@ -83,10 +164,10 @@ def main():
         st.header("Backtest Results")
 
         # Create a CustomBacktest instance
-        backtest = utils.CustomBacktest(
+        backtest = CustomBacktest(
             initial_date=datetime.combine(initial_date, datetime.min.time()),
             final_date=datetime.combine(final_date, datetime.min.time()),
-            information_class=utils.CustomFirstTwoMoments,
+            information_class=CustomFirstTwoMoments,
             risk_model=StopLoss,
             name_blockchain="backtest",
             verbose=verbose,
@@ -385,7 +466,7 @@ def main():
             else:
                 st.write("The blockchain is not valid.")
             st.write("Below is the raw blockchain data:")
-            for i in utils.split_text_by_lines(str(block_chain)):
+            for i in split_text_by_lines(str(block_chain)):
                 st.markdown(
                     f"<div style='font-size:12px; font-family:monospace;'>{i}</div>",
                     unsafe_allow_html=True,
